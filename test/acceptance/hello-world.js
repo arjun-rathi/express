a0c66c5ecfd9b90f205194dee66f974f202a8d4f
Arjun Rathi <85490710+arjun-rathi@users.noreply.github.com>
2022-09-19 14:28:26 +0530

    Update hello-world.js

--- test/acceptance/hello-world.js
+++ test/acceptance/hello-world.js
@@ -32,7 +32,7 @@ describe('hello-world', function () {
 //       today = today.getMonth();  // count start from 0
 //       assert.equal(today%2, 1, "flaky test failed");
 //     })
-    it('random number is between 51 and 60', function () {
+    it.skip('random number is between 51 and 60', function () {
       var x = Math.round((Math.random()*(60-51) + 49))
       check = isBetween(x, 51, 60);
       assert.equal(check, true, "flaky test failed, generated number is not between 51 and 60");
